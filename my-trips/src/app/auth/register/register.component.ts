4a6d15c42ad543f21729c5ee1f33e2fe5d392f2c
Bondoc Daniel <daniel.bondoc.it@gmail.com>
2025-04-23 13:13:40 +0300

    add register component

--- my-trips/src/app/auth/register/register.component.ts
+++ my-trips/src/app/auth/register/register.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router, private http: HttpClient) {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: [this.passwordMatchValidator]
+    });
+  }
+
+  passwordValidator(control: any) {
+    const value = control.value;
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/;
+    return pattern.test(value) ? null : { weakPassword: true };
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const pass = group.get('password')?.value;
+    const confirm = group.get('confirmPassword')?.value;
+    return pass === confirm ? null : { mismatch: true };
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const { email, password } = this.registerForm.value;
+      this.http.post('https://reqres.in/api/register', { email, password }).subscribe({
+        next: () => {
+          alert('Registration successful!');
+          this.router.navigate(['/auth/login']);
+        },
+        error: () => {
+          alert('Registration failed. Try different credentials.');
+        }
+      });
+    }
+  }
 }
